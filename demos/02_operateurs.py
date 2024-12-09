15acd71bba96651959ea4a34877db653e7b6404c
Christophe-Utopios <christophe.r@utopios.solutions>
2024-12-09 12:25:58 +0100

    Opérateurs arithmétiques

---
+++ demos/02_operateurs.py
@@ -0,0 +1,31 @@
+# Les opérateurs arithmétique
+
+# Addition :
+a = 10 + 15
+# Division :
+a = 10 / 15
+# Pour une division entière (sans virgule)
+a = 10 // 15
+# Modulo (le reste de la division)
+a = 10 % 15
+# Multiplication
+a = 10 * 15
+# Puissance de ...
+a = 15 ** 10
+# Soustraction :
+a = 15 - 10
+
+b = 10
+# Solution 1 :
+b = b - 5
+# Solution 2 :
+b -= 5
+b *= 2
+
+nba = input("Saisir un nombre a : ")
+nbb = input("Saisir un nombre b : ")
+# Concaténation de "1" + "1"
+print(nba + nbb)
+
+# La multiplication sur une chaîne de caractères duplique celle-ci.
+print(nba * 10)
