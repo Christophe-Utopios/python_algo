49590e1c888f8352195a00d56acb5249bd45e1ea
Christophe-Utopios <christophe.r@utopios.solutions>
2024-12-11 16:36:31 +0100

    Correction exercices liste

---
+++ exercices/exercice_18/exercice_18.py
@@ -0,0 +1,6 @@
+# ma_liste = [1,2,3,4,5,6,7,8,9,10]
+
+# Liste comprehension
+ma_liste = [i for i in range(1, 11)]
+
+print(ma_liste[8])
---
+++ exercices/exercice_19/exercice_19.py
@@ -0,0 +1,8 @@
+ma_liste = []
+
+for i in range(1, 16):
+    nb = int(input(f"Veuillez saisir la note n°{i} : "))
+    ma_liste.append(nb)
+
+
+print(ma_liste)
