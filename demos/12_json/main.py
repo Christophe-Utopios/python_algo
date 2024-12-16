a0adbbd9010d34038b7260752a8595e0a405f6f9
Christophe-Utopios <christophe.r@utopios.solutions>
2024-12-16 13:42:53 +0100

    Demo JSON

--- demos/12_json/main.py
+++ demos/12_json/main.py
@@ -6,9 +6,22 @@ file_path = "./demos/12_json/personnes.json"
 with open(file_path, "r", encoding="UTF-8") as fichier:
     personnes : list = json.load(fichier)
 
-personnes.append({"Nom": "Titi", "Email": "titi@email.fr", "Age": 24})
+# personnes.append({"Nom": "Titi", "Email": "titi@email.fr", "Age": 24})
 
 # La méthode .dump() permet d'écrre dans mon fichier json
-with open(file_path, "w", encoding="UTF-8") as fichier:
-    # indent sert à avoir une présentation plus esthétique
-    json.dump(personnes, fichier, indent=4)
+# with open(file_path, "w", encoding="UTF-8") as fichier:
+#     # indent sert à avoir une présentation plus esthétique
+#     json.dump(personnes, fichier, indent=4)
+
+
+# Pour obtenir la variable qui va être le représentation textuelle d'un dictionnaire
+# on peut se servir de la méthode .dumps()
+json_str = json.dumps(personnes, indent=4)
+print(json_str)
+print(type(json_str))
+
+# Pour transformer une chaîne de caractère au format JSON en dictionnaire, il existe la méthode 
+# .loads()
+data = json.loads(json_str)
+print(data)
+print(type(data))
